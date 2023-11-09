b37c0447b277d5d0d7fb0f0ac473175e6c389857
Alan Smith <commit@alanwsmith.com>
2023-11-08 20:53:54 -0500

    update files

--- test-1/startup.lua
+++ test-1/startup.lua
@@ -1,1 +1,6 @@
-print("this is from github")
+local response = http.get("https://raw.githubusercontent.com/alanwsmith/minecraft-lua/main/test-1/index.lua")
+local status = response.getResponseCode()
+
+if status == 200 then
+  print("Got main.lua")
+end
